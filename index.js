e83ae4f2da4ec6a265ef2ce97d7c2ffbcf83c04d
antoniel13dasilva-blip <antoniel13dasilva@gmail.com>
2026-03-09 16:01:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,43 @@ function createBot() {
 
 createBot()
 
-const http = require('http');
+
+const mineflayer = require('mineflayer')
+const http = require('http')
+
+// Cria um servidor web para o Render/UptimeRobot não desligarem o bot
 http.createServer((req, res) => {
-    res.write("Bot está online!");
-    res.end();
-}).listen(8080);
+    res.write("Bot está online!")
+    res.end()
+}).listen(8080)
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'Server777891.aternos.me', 
+        port: 18749, 
+        username: 'Bot24Horas', 
+        version: '1.21.11'
+    })
+
+    bot.on('login', () => {
+        console.log('Bot conectado!')
+        // Faz o bot mandar uma mensagem no chat assim que entrar
+        bot.chat('Bot 24h ativo e cuidando do servidor!')
+        
+        // Faz o bot pular a cada 60 segundos para não ser expulso
+        setInterval(() => {
+            bot.setControlState('jump', true)
+            setTimeout(() => bot.setControlState('jump', false), 500)
+        }, 60000)
+    })
+
+    bot.on('error', (err) => console.log('Erro:', err.message))
+
+    bot.on('end', () => {
+        console.log('Conexão perdida, tentando reconectar em 10 segundos...')
+        setTimeout(createBot, 10000)
+    })
+}
+
+createBot()
+
